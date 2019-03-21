64649801a18f4bc953d148bee16956fdd6e4da65
gabsgeorge <gabriel.g.s.santos@hotmail.com>
2019-03-21 10:41:46 -0300

    up

---
+++ qlikportal/core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView, CreateView, UpdateView, FormView, ListView
+
+
+# Create your views here.
+class IndexView(TemplateView):
+	template_name = 'index.html'
+
+class TabView(TemplateView):
+	template_name = 'tab.html'
+    
---
+++ qlikportal/qlikportal/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+from django.conf.urls import url, include
+
+urlpatterns = [
+
+	path('admin/', admin.site.urls, name='admin'),
+
+	path('', views.IndexView.as_view(), name='index'),
+
+	path('index/', views.IndexView.as_view(), name='index'),
+
+	path('tab/', views.TabView.as_view(), name='tab'),
+
+
+
+	]
+
+
